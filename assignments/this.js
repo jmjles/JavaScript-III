84fe3c9be8df663da750fadc83755fe253dd36f5
jmjles <jmjles@gmail.com>
2019-10-17 12:42:49 -0700

    Revert "mvp completed"
    This reverts commit 9434b48b6532e22162e5b6a21f07b373eae263ce.

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. This is refering to the global (window) scope.
-* 2. This is refering to the same scope that it's in.
-* 3. New refers to whatever is being created (whatever is on the righthand side).
-* 4. This is being refered and directed in an external call.
+* 1. 
+* 2. 
+* 3. 
+* 4. 
 *
 * write out a code example of each explanation above
 */
@@ -12,43 +12,15 @@
 // Principle 1
 
 // code example for Window Binding
-window.msg= 'Global Binding'
-console.log(this.msg)
 
 // Principle 2
 
 // code example for Implicit Binding
-const animal={
-    name:'Betsy',
-    sound:'Mooo',
-    color:'black and white',
-    age: 13,
-    intro: function(){
-        return `My name is ${this.name} and I'm ${this.age} years old! I have ${this.color} hair and I make the sound "${this.sound}"`
-    }
-}
-console.log(animal.intro());
+
 // Principle 3
 
 // code example for New Binding
-const name = function(first_name,last_name){
-    this.first_name = first_name,
-    this.last_name = last_name
-    return `${first_name} ${last_name}`
-}
-let jesus= new name('Jesus','Morales')
-
-console.log(jesus)
 
 // Principle 4
 
 // code example for Explicit Binding
-function car(make,model){
-    return console.log(`${this.name} owns a ${make} ${model}`);
-}
-const bmw = ['BMW', 'I330'];
-let owner ={
-    name:'Jesus'
-};
-
-car.apply(owner,bmw);
